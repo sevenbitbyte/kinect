7aecc6d38ebb1540afbe41baceafdfbbe9273975
Alan Meekins <alan.meekins@gmail.com>
2013-07-10 20:22:38 -0700

    Patched kinect_aux to interpolate accel data in each axis independantly as well as pass the data through a median filter to remove noise. This patch also implements clipping to prevent output of accel readings that include excessive accelerations.

--- kinect_aux/src/kinect_aux.cpp
+++ kinect_aux/src/kinect_aux.cpp
@@ -1,20 +1,309 @@
 #include <libusb.h>
 #include <ros/ros.h>
+#include <ros/console.h>
 #include <std_msgs/UInt8.h>
 #include <std_msgs/UInt16.h>
 #include <std_msgs/Float64.h>
 #include <sensor_msgs/Imu.h>
+
+#include <tf/tf.h>
+
+#include <map>
+#include <vector>
 
 // VID and PID for Kinect and motor/acc/leds
 #define MS_MAGIC_VENDOR 0x45e
 #define MS_MAGIC_MOTOR_PRODUCT 0x02b0
 // Constants for accelerometers
 #define GRAVITY 9.80665
-#define FREENECT_COUNTS_PER_G 819.
+#define FREENECT_COUNTS_PER_G 819.0
+#define FREENECT_COUNTS_PER_G_X 850.0
+#define FREENECT_COUNTS_PER_G_Y 850.0
+
+#define FREENECT_COUNTS_PER_G_X_MAX 877.5
+#define FREENECT_COUNTS_PER_G_X_ZERO 37.0
+#define FREENECT_COUNTS_PER_G_X_MIN -795.0
+
+#define FREENECT_COUNTS_PER_G_Y_MAX 861.0
+#define FREENECT_COUNTS_PER_G_Y_ZERO 15.0
+#define FREENECT_COUNTS_PER_G_Y_MIN -811.0
+
+#define FREENECT_COUNTS_PER_G_Z_MAX 834.0
+#define FREENECT_COUNTS_PER_G_Z_ZERO 3.0
+#define FREENECT_COUNTS_PER_G_Z_MIN -823.5
+
+#define ERROR_MARGIN	0.90
+
+using namespace std;
+
+
+struct Point2d {
+	double x;
+	double y;
+
+	Point2d(){
+		x = 0.0;
+		y = 0.0;
+	}
+
+	Point2d(double x, double y){
+		this->x = x;
+		this->y = y;
+	}
+};
+
+struct Line{
+	Point2d start;
+	Point2d end;
+	double m;
+	double b;
+
+	Line(Point2d pt1, Point2d pt2){
+		this->start = pt1;
+		this->end = pt2;
+
+		m = (start.y - end.y) / (start.x - end.x);
+		b = end.y - (m * end.x);
+	}
+
+	void recompute(){
+		m = (start.y - end.y) / (start.x - end.x);
+		b = end.y - (m * end.x);
+	}
+
+	double getY(double x){
+		return (m*x) + b;
+	}
+};
+
+class Interpolator{
+	public:
+		vector<Line*> lines;
+
+		Interpolator(){
+			lines.clear();
+		}
+
+		Interpolator(Point2d start, Point2d end){
+			lines.push_back(new Line(start, end) );
+		}
+
+		Interpolator(double x1, double y1, double x2, double y2){
+			lines.push_back(new Line(Point2d(x1, y1), Point2d(x2, y2)) );
+		}
+
+		~Interpolator(){
+			while(!lines.empty()){
+				if(lines.back() != NULL){
+					delete lines.back();
+				}
+
+				lines.pop_back();
+			}
+		}
+
+		void addLine(Point2d pt1, Point2d pt2){
+			lines.push_back(new Line(pt1, pt2) );
+		}
+
+		void recompute(){
+			vector<Line*>::const_iterator lineIter = lines.begin();
+
+			for(; lineIter != lines.end(); lineIter++){
+				(*lineIter)->recompute();
+			}
+		}
+
+		Line* getNearestLine(double x){
+			vector<Line*>::const_iterator lineIter = lines.begin();
+			Line* bestLine = NULL;
+			double distance = -1;
+
+			for(; lineIter != lines.end(); lineIter++){
+				//Test start
+				double startDelta = (*lineIter)->start.x - x;
+
+				ROS_DEBUG("getNearestLine(%f): start delta = %f", x, startDelta);
+
+				if(distance == -1 || abs(startDelta) < distance){
+					distance = abs(startDelta);
+					bestLine = *lineIter;
+				}
+
+				//Test end
+				double endDelta = (*lineIter)->end.x - x;
+
+				ROS_DEBUG("getNearestLine(%f): end delta = %f", x, endDelta);
+
+				if(distance == -1 || abs(endDelta) < distance){
+					distance = abs(endDelta);
+					bestLine = *lineIter;
+				}
+
+				//Within bounds?
+				if((startDelta < 0 && endDelta > 0) || (startDelta > 0 && endDelta < 0)){
+					//Point falls directly within bounds, this is always the best line to select
+					distance = abs(endDelta);
+					bestLine = *lineIter;
+				}
+			}
+
+			ROS_DEBUG("getNearestLine(%f): returned Line{ (%f, %f), (%f, %f) }", x, bestLine->start.x, bestLine->start.y, bestLine->end.x, bestLine->end.y);
+
+			return bestLine;
+		}
+
+		double getValue(double x){
+			Line* line = getNearestLine(x);
+			double y = line->getY(x);
+
+			//TODO: Want to filter here at all?
+
+			return y;
+		}
+};
+
+class Filter {
+    public:
+		unsigned int bufferLength;
+		unsigned int mediansLength;
+
+		vector<double> kernel;
+		vector<double> data;
+		vector<double> medians;
+		multimap<double,double> medianMap;
+
+		enum FilterType { Filter_Average, Filter_Median };
+
+		uint8_t type;
+
+		Filter(){
+			bufferLength = 300;
+			mediansLength = 50;
+			data.clear();
+			kernel.clear();
+			medians.clear();
+
+			type = Filter_Median;
+
+		}
+
+		Filter(int dataLength){
+			bufferLength = dataLength;
+			data.clear();
+			kernel.clear();
+			medians.clear();
+		}
+
+		double addKernel(double value){
+			kernel.push_back(value);
+		}
+
+		double getValue(double value){
+
+
+            if(type == Filter_Median){
+                data.push_back(value);
+
+				medianMap.clear();
+				for(int i=0; i < data.size(); i++){
+                    medianMap.insert(make_pair(data[i], data[i]));
+                }
+
+				double median = getValue();
+
+				medians.push_back(median);
+
+				if(medians.size() > mediansLength){
+					medians.erase(medians.begin());
+				}
+
+				value = 0.0;
+				for(unsigned int i=0; i<medians.size(); i++){
+					value += medians[i];
+				}
+
+				value = value / (double) medians.size();
+			}
+			else if(type == Filter_Average){
+				data.push_back(value);
+				value = getValue();
+			}
+
+
+			if(data.size() > bufferLength){
+				data.erase(data.begin());
+			}
+
+
+			return value;
+		}
+
+		double getValue(){
+			double value = 0.0;
+
+			if(type == Filter_Average){
+				vector<double>::iterator kernelIter = kernel.begin();
+				vector<double>::iterator dataIter = data.end();
+
+				for(int i=0; i < data.size(); i++){
+					double datam = data[i];
+
+					if(kernel.size() != 0){
+						value += datam * (*kernelIter);
+
+						kernelIter++;
+
+						if(kernelIter == kernel.end()){
+							kernelIter = kernel.begin();
+						}
+					}
+					else{
+						value += datam;
+					}
+					dataIter--;
+				}
+
+				if(type==Filter_Average && kernel.size() == 0){
+					value = value / (double) bufferLength;
+				}
+			}
+			else if(type == Filter_Median){
+				//select median value
+				int middle = medianMap.size()/2;
+
+				map<double,double>::iterator medianIter = medianMap.begin();
+				for(int i=0; medianIter != medianMap.end(); medianIter++,i++){
+					if(i==middle){
+						value = medianIter->second;
+						break;
+					}
+				}
+			}
+
+			return value;
+		}
+};
+
+#define MIN_INDEX	0
+#define ZERO_INDEX	1
+#define MAX_INDEX	2
+
+#define XAxisRanges { FREENECT_COUNTS_PER_G_X_MIN, FREENECT_COUNTS_PER_G_X_ZERO, FREENECT_COUNTS_PER_G_X_MAX }
+#define YAxisRanges { FREENECT_COUNTS_PER_G_Y_MIN, FREENECT_COUNTS_PER_G_Y_ZERO, FREENECT_COUNTS_PER_G_Y_MAX }
+#define ZAxisRanges { FREENECT_COUNTS_PER_G_Z_MIN, FREENECT_COUNTS_PER_G_Z_ZERO, FREENECT_COUNTS_PER_G_Z_MAX }
+
+uint16_t selected_axis = 0;	//! Used by topic /imu/debug/axis_selector
+double axes[3][3] = { XAxisRanges, YAxisRanges, ZAxisRanges };
+
+
+Interpolator interpolators[3];
+Filter filters[3];
+
 // The kinect can tilt from +31 to -31 degrees in what looks like 1 degree increments
 // The control input looks like 2*desired_degrees
-#define MAX_TILT_ANGLE 31.
-#define MIN_TILT_ANGLE (-31.)
+#define MAX_TILT_ANGLE 31.0
+#define MIN_TILT_ANGLE (-31.0)
 
 ros::Publisher pub_imu;
 ros::Publisher pub_tilt_angle;
@@ -22,7 +311,12 @@ ros::Publisher pub_tilt_status;
 
 ros::Subscriber sub_tilt_angle;
 ros::Subscriber sub_led_option;
- 
+
+ros::Subscriber sub_axis_selector;
+ros::Subscriber sub_axis_max;
+ros::Subscriber sub_axis_zero;
+ros::Subscriber sub_axis_min;
+
 libusb_device_handle *dev(0);
 
 void openAuxDevice(int index = 0)
@@ -66,6 +360,10 @@ void openAuxDevice(int index = 0)
 	libusb_free_device_list (devs, 1);  // free the list, unref the devices in it
 }
 
+double lastRoll = 0.0;
+double lastPitch = 0.0;
+double lastForce = 0.0;
+Filter deltaFilter;
 
 void publishState(void)
 {
@@ -81,9 +379,9 @@ void publishState(void)
 	const uint16_t uy = ((uint16_t)buf[4] << 8) | buf[5];
 	const uint16_t uz = ((uint16_t)buf[6] << 8) | buf[7];
 	
-	const int16_t accelerometer_x = (int16_t)ux;
-	const int16_t accelerometer_y = (int16_t)uy;
-	const int16_t accelerometer_z = (int16_t)uz;
+	const double accelerometer_x = (int16_t)ux;
+	const double accelerometer_y = (int16_t)uy;
+	const double accelerometer_z = ((int16_t)uz);
 	const int8_t tilt_angle = (int8_t)buf[8];
 	const uint8_t tilt_status = buf[9];
 	
@@ -92,14 +390,44 @@ void publishState(void)
 	if (pub_imu.getNumSubscribers() > 0)
 	{
 		imu_msg.header.stamp = ros::Time::now();
-		imu_msg.linear_acceleration.x = (double(accelerometer_x)/FREENECT_COUNTS_PER_G)*GRAVITY;
-		imu_msg.linear_acceleration.y = (double(accelerometer_y)/FREENECT_COUNTS_PER_G)*GRAVITY;
-		imu_msg.linear_acceleration.z = (double(accelerometer_z)/FREENECT_COUNTS_PER_G)*GRAVITY;
+
+
+		imu_msg.linear_acceleration.x = filters[0].getValue(interpolators[0].getValue(accelerometer_x) * GRAVITY);
+		imu_msg.linear_acceleration.y = filters[1].getValue(interpolators[1].getValue(accelerometer_y) * GRAVITY);
+		imu_msg.linear_acceleration.z = filters[2].getValue(interpolators[2].getValue(accelerometer_z) * GRAVITY);
+
+
+
 		imu_msg.linear_acceleration_covariance[0] = imu_msg.linear_acceleration_covariance[4]
 			= imu_msg.linear_acceleration_covariance[8] = 0.01; // @todo - what should these be?
 		imu_msg.angular_velocity_covariance[0] = -1; // indicates angular velocity not provided
 		imu_msg.orientation_covariance[0] = -1; // indicates orientation not provided
-		pub_imu.publish(imu_msg);
+
+		double magnitude = sqrt((imu_msg.linear_acceleration.x * imu_msg.linear_acceleration.x) + (imu_msg.linear_acceleration.y * imu_msg.linear_acceleration.y) + (imu_msg.linear_acceleration.z + imu_msg.linear_acceleration.z));
+
+		//Only publish when the summed magnitued is within 15% of the expected force due to gravity
+		double error = magnitude - GRAVITY;
+		double delta = magnitude - lastForce;
+        double deltaExpected = deltaFilter.getValue(delta);
+
+		if(abs(delta - deltaExpected) < (0.1 * GRAVITY) && abs(error) < (GRAVITY * ERROR_MARGIN)){
+			tf::Quaternion q;
+
+			double pitch = -atan2( imu_msg.linear_acceleration.z, sqrt((imu_msg.linear_acceleration.x * imu_msg.linear_acceleration.x)  + (imu_msg.linear_acceleration.y * imu_msg.linear_acceleration.y)) );
+			double roll = atan2( imu_msg.linear_acceleration.x, sqrt((imu_msg.linear_acceleration.z * imu_msg.linear_acceleration.z)  + (imu_msg.linear_acceleration.y * imu_msg.linear_acceleration.y)) );
+
+
+            q.setRPY( roll, pitch, 0.0 );
+
+			imu_msg.header.frame_id = "/camera_link";
+
+			imu_msg.orientation.x = q.x();
+			imu_msg.orientation.y = q.y();
+			imu_msg.orientation.z = q.z();
+			imu_msg.orientation.w = q.w();
+
+			pub_imu.publish(imu_msg);
+        }
 	}
 	
 	// publish tilt angle and status
@@ -146,6 +474,37 @@ void setLedOption(const std_msgs::UInt16 optionMsg)
 	}
 }
 
+void setAxisSelectorCallback(const std_msgs::UInt16 axisId){
+	selected_axis = axisId.data;
+}
+
+void setAxisMaxCallback(std_msgs::Float64 max){
+	interpolators[selected_axis].lines[1]->end.x = max.data;
+
+	interpolators[selected_axis].recompute();
+}
+
+void setAxisMinCallback(std_msgs::Float64 min){
+	if(min.data > 0){
+		interpolators[selected_axis].lines[0]->start.x = -min.data;
+	}else{
+		interpolators[selected_axis].lines[0]->start.x = min.data;
+	}
+
+	interpolators[selected_axis].recompute();
+}
+
+void setAxisZeroCallback(std_msgs::Float64 zero){
+
+	if(selected_axis == 1){
+		zero.data = -zero.data;
+	}
+
+	interpolators[selected_axis].lines[0]->end.x = zero.data;
+	interpolators[selected_axis].lines[1]->start.x = zero.data;
+
+	interpolators[selected_axis].recompute();
+}
 
 int main(int argc, char* argv[])
 {
@@ -159,6 +518,25 @@ int main(int argc, char* argv[])
 	ros::init(argc, argv, "kinect_aux");
 	ros::NodeHandle n;
 	
+
+	deltaFilter.type = Filter::Filter_Median;
+	deltaFilter.bufferLength = 300;
+	deltaFilter.mediansLength = 5;
+
+    for(int i=0; i < 3; i++){
+		Interpolator* interp = &interpolators[i];
+
+		Point2d start(axes[i][0], -1.0);
+		Point2d zero(axes[i][1], 0.0);
+		Point2d end(axes[i][2], 1.0);
+
+
+		interp->addLine( start, zero );
+		interp->addLine( zero, end );
+	}
+
+
+
 	int deviceIndex;
 	n.param<int>("device_index", deviceIndex, 0);
 	openAuxDevice(deviceIndex);
@@ -175,7 +553,12 @@ int main(int argc, char* argv[])
 	
 	sub_tilt_angle = n.subscribe("tilt_angle", 1, setTiltAngle);
 	sub_led_option = n.subscribe("led_option", 1, setLedOption);
-	 
+
+	sub_axis_selector = n.subscribe("/imu/debug/axis_selector", 1, setAxisSelectorCallback);
+	sub_axis_min = n.subscribe("/imu/debug/axis_min", 1, setAxisMinCallback);
+	sub_axis_zero = n.subscribe("/imu/debug/axis_zero", 1, setAxisZeroCallback);
+	sub_axis_max = n.subscribe("/imu/debug/axis_max", 1, setAxisMaxCallback);
+
 	while (ros::ok())
 	{
 		ros::spinOnce();
